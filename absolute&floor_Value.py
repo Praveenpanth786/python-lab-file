c11bb93f77c4bf5560fbb6cec44f95e55c1f9abc
Praveen Kumar  Panth <93048557+Praveenpanth786@users.noreply.github.com>
2023-09-26 18:06:28 +0530

    Add files via upload

---
+++ absolute&floor_Value.py
@@ -0,0 +1,14 @@
+import math
+absolute = -5.999 
+floor_test = 198.57
+ceil_test=198.57
+
+
+result1 = math.fabs(absolute)
+result2 = math.floor(floor_test)
+result3 = math.ceil(ceil_test)
+print(result1, " is the absolute value of ", absolute)
+print(result2, " is the flow of ", floor_test)
+print(result3, " is the flow of ", ceil_test)
+print("the power of given no. is: ",math.pow(2, 3))
+print("the gcd of given no. is: ",math.gcd(100, 24))
---
+++ math.py
@@ -0,0 +1,43 @@
+print("first code:-----------")
+import math
+print(math.sqrt(16))
+print(math.pi)
+
+
+print(math.sqrt(20))
+print(math.pi)
+
+
+print(math.sqrt(30))
+print(math.pi)
+
+print("second code:-----------")
+import math as m
+print(m.sqrt(16))
+print(m.pi)
+
+print(m.sqrt(10))
+print(m.pi)
+
+
+print("third code:-----------")
+from math import sqrt,pi
+print(sqrt(16))
+print(pi)
+
+
+print("fourth code:-----------")
+from math import pi
+r=16
+print("Area of Cicle is:",pi*r**2)
+
+
+from math import pi
+r=16
+print("Perametre of Cicle is:",2*pi*r)
+
+d=32
+print("Perametre of Cicle with diameter is:",pi*d)
+
+print("fifth code:-----------")
+print("factorial of given no is: ", math.factorial(4))
